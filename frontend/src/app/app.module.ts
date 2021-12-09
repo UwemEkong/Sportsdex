247705f34c08cca929243997169b7670bc7cb7dd
UwemEkong <uweme6@gmail.com>
2021-12-09 16:38:24 -0600

    initial files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AchievementFilterComponent } from './components/achievement-filter/achievement-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AchievementFilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/achievement-filter/achievement-filter.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-achievement-filter',
+  templateUrl: './achievement-filter.component.html',
+  styleUrls: ['./achievement-filter.component.scss']
+})
+export class AchievementFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  filterAchievements(filterData: any) {
+
+  }
+
+}
