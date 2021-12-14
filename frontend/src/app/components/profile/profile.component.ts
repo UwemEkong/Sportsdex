2bd50139a453253711c41e6648d77e55b6582a8d
AdamChaplin23 <b2482993@ben.edu>
2021-12-14 15:48:59 -0600

    updated profile shite

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthServService} from "../../services/auth-serv.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(public authServivce: AuthServService) {
+    this.authServivce = authServivce;
+  }
 
   ngOnInit(): void {
   }
